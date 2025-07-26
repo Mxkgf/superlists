72fb686cab46ee2fa0618c0273c9e8d48044763b
maxiaokang <maxiukang@qq.com>
2025-07-26 17:22:42 +0800

    Functional test now checks we can save a POST request

--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, "home.xhtml")
+    return render(request, "home.xhtml", {"new_item_text": request.POST.get("item_text")})
