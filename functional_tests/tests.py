ab2c3bb8e7c99de36081cb327340977c04c7c43e
maxiaokang <maxiukang@qq.com>
2025-08-08 12:15:37 +0800

    Extract new_list view to handle POST requests
    - Move item creation from home_page to new_list view
    - Add /lists/new URL endpoint
    - Update form action in home.html template
    - Refactor associated tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -19,7 +19,7 @@ class NewVistorTest(LiveServerTestCase):
         start_time = time.time()
         while True:
             try:
-                table = self.browser.find_element_by_id("id_list_table")
+                table = self.browser.find_element(By.ID, "id_list_table")
                 rows = table.find_elements_by_tag_name("tr")
                 self.assertIn(row_text, [row.text for row in rows])
                 return
@@ -58,7 +58,7 @@ class NewVistorTest(LiveServerTestCase):
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
         # 伊迪丝做事很有条理
-        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
 
