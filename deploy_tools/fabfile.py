f88279f676c01e623006e93af1179633ec51caed
maxiaokang <maxiukang@qq.com>
2025-08-12 15:04:04 +0800

    Fix deploy()

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,11 +5,12 @@ import random
 REPO_URL = "https://github.com/Mxkgf/superlists.git"
 
 def deploy():
-    site_folder = f"/home/{env.user}/sites/{'superlists-staging.maxiaokang.website'}"
+    site_name = "superlists-staging.maxiaokang.website"
+    site_folder = f"/home/{env.user}/sites/{site_name}"
     source_folder = site_folder + "/source"
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
-    _update_settings(source_folder, env.host)
+    _update_settings(source_folder, site_name)
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
